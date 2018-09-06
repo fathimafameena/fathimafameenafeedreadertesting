7805bcf989900d0273b41411e8475b064f156ffc
fathimafameena <fathima.fameena@yahoo.com>
2018-09-06 11:08:23 -0500

    added more detailed comments

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -23,7 +23,7 @@ $(function() {
          */
             it('are defined', function() {
                 expect(allFeeds).toBeDefined();
-                expect(allFeeds.length).not.toBe(0);
+                expect(allFeeds.length).not.toBe(0);/*checking the allfeeds variable is not empty*/
             });
 
 
@@ -35,7 +35,7 @@ $(function() {
 
                 for(var i=0;i<allFeeds.length;i++){
                     expect(allFeeds[i].url).toBeDefined();
-                    expect(allFeeds[i].url.length).not.toBe(0);
+                    expect(allFeeds[i].url.length).not.toBe(0);/*checking the allfeeds url is not empty*/
                 }
           });
 
@@ -47,7 +47,7 @@ $(function() {
          it('name defined',function(){
                 for(var i=0;i<allFeeds.length;i++){
                      expect(allFeeds[i].name).toBeDefined();
-                     expect(allFeeds[i].name.length).not.toBe(0);
+                     expect(allFeeds[i].name.length).not.toBe(0);/*checking the allfeeds name is not empty*/
                 }
           });
     });
@@ -60,14 +60,14 @@ $(function() {
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
          */
-        var hidden,menuicon;
+        var mybody,menuicon;
         beforeEach(function(){
-            mybody=document.getElementById('mybody');
+            mybody=document.getElementById('mybody');/*accesing the body element using mybody variable*/
             menuicon=document.getElementById('menu-icon');
 
         });
         it('Does the menu hidden by default',function(){
-            expect(mybody.classList.contains('menu-hidden')).toBe(true);
+            expect(mybody.classList.contains('menu-hidden')).toBe(true);//checking whether the body element contains the class//
         });
          
          /* TODO: Write a test that ensures the menu changes
@@ -76,9 +76,9 @@ $(function() {
           * clicked and does it hide when clicked again.
           */
         it('Does the menu changes visibility when the menu icon is clicked',function(){
-            menuicon.click();
+            menuicon.click();/*on the first click to the menu the list appears*/
             expect(mybody.classList.contains('menu-hidden')).toBe(false);
-            menuicon.click();
+            menuicon.click();/*on the next click the menu list should disappear*/
             expect(mybody.classList.contains('menu-hidden')).toBe(true);
 
         });
@@ -91,19 +91,19 @@ $(function() {
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
-         var feed=document.getElementById('feed');
+         var feed=document.getElementById('feed');/*accessing the feed  div element  in the feed variable*/
          var entry=document.getElementById("entryid");
          beforeEach(function(done){
-            loadFeed(0,function(){
+            loadFeed(0,function(){/* calling the load feed icon once*/
             done();
             console.log(feed.childNodes.length);
             });
         });
  
-        it('atleast single element in feed contain',function(){
+        it('atleast single element in feed',function(){
             expect(feed).toBeDefined();
             expect(entry).toBeDefined();
-            expect(feed.childNodes.length).not.toBe(0);
+            expect(feed.childNodes.length).not.toBe(0);/*checking if atleast one element in feed after running the load feed function*/
         });
     });
     /* TODO: Write a new test suite named "New Feed Selection" */
@@ -116,19 +116,19 @@ $(function() {
         var firstfeedurl,secondfeedurl;
         var entry=document.getElementById("entryid");
         beforeEach(function(done){
-            loadFeed(0,function(){
-            firstfeedurl=document.querySelector('.header-title').innerHTML;
+            loadFeed(0,function(){/*running the loadfeed function once */
+            firstfeedurl=document.querySelector('.header-title').innerHTML;/*storing the first feed heading  in the firstfeed variable*/
             console.log(firstfeedurl);
             });
-            loadFeed(1,function(){
+            loadFeed(1,function(){/*running the loadfeed function again with the different id*/
             done();
-            secondfeedurl=document.querySelector('.header-title').innerHTML;
+            secondfeedurl=document.querySelector('.header-title').innerHTML;/*storing the second feed heading in an another variable*/
             console.log(secondfeedurl);
             });
         });
  
-        it('it changes the feed',function(){
-            expect(firstfeedurl===secondfeedurl).toBe(false);
+        it('it changes the feed content',function(){
+            expect(firstfeedurl===secondfeedurl).toBe(false);/*checking if both should not be same*/
         });
     });
 }());
